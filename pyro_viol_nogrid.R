1d72b3cd5ec516f5fe236c88f7d63eca33e3a242
Sen <phd.geek@gmail.com>
2016-10-26 11:54:42 +0200

    Added plot and data files

---
+++ pyro_viol_nogrid.R
@@ -0,0 +1,95 @@
+# Senthil / UNLV / May 27 2015
+# This version includes Coleman et al., YNP samples
+library(plyr)
+library(reshape2)
+library(ggplot2)
+mydat <- read.table('may27_dcol.csv',
+                    head=T, row.names=NULL, sep='\t')
+melt_mydat<-melt(mydat)
+
+## Violin plots for YNP, WC, Armenia, Tibet, Tengchong, NZ, Phil,
+## Guaymas Bay, GBS samples. Final version:
+## http://www.ncbi.nlm.nih.gov/pubmed/26113243
+
+## Some samples are organized by sites, others by region
+sitelist <- c('Yellowstone National Park',
+              'Great Basin', 'Guaymas Bay')
+regionlist <- c('Canada', 'Armenia', 'Tibet', 'China', 'New Zealand',
+                 'Philippines')
+
+
+for (i in seq(sitelist)){
+    dat <- subset(melt_mydat, Site == sitelist[[i]])
+    pdffile <- paste(gsub(' ', '_', sitelist[[i]]),
+                     '_may27.pdf', sep='')
+    pdf(pdffile, width=8, height=5, useDingbats=FALSE)
+    (p <- ggplot(dat, aes(x=variable, y=value*100)) +
+     geom_violin(aes(fill=factor(variable)), scale='width') +
+     geom_boxplot(aes(fill=factor(variable)), width=0.1) +
+     scale_fill_manual(values = c('#00C957', '#1E90FF',
+                           '#FFA500', '#DC143C')) +
+     scale_y_continuous(limits=c(0, 100)) +
+     ylab('% Unclassified') +
+     stat_summary(fun.y = 'mean', geom = 'point',
+                  shape=15, size=4, fill= 'white') +
+     coord_flip())
+
+    if (sitelist[[i]] == 'Guaymas Bay'){
+        (p <- ggplot(dat, aes(x=variable, y=value*100)) +
+         geom_violin(aes(fill=factor(variable)), scale='width') +
+         geom_boxplot(aes(fill=factor(variable)), width=0.1) +
+         scale_fill_manual(values = c('#00C957', '#1E90FF',
+                               '#FFA500', '#DC143C')) +
+         ylab('% Unclassified') +
+         stat_summary(fun.y = 'mean', geom = 'point',
+                      shape=15, size=4, fill= 'white') +
+         coord_flip())
+    }
+    print(p + theme(panel.background=element_blank(),
+                    panel.border=element_blank(),
+                    panel.grid.minor=element_blank(),
+                    panel.grid.major=element_blank(),
+                    axis.line=element_line(),
+                    axis.text.x=element_text(size=30, face='bold'),
+                    axis.text.y=element_text(size=30, face='bold'),
+                    axis.title.x=element_text(size=30, face='bold',
+                        vjust=0.2),
+                    axis.title.y=element_blank(),
+                    legend.position='none'))
+    dev.off()
+    rm(p)
+}
+
+for (i in seq(regionlist)){
+    dat <- subset(melt_mydat, Region == regionlist[[i]])
+    pdffile <- paste(gsub(' ', '_', regionlist[[i]]),
+                     '_may27.pdf', sep='')
+    if (regionlist[[i]] == 'China'){
+        pdffile <- 'Tengchong_may27.pdf'
+    }
+    pdf(pdffile, width=8, height=5, useDingbats=FALSE)
+    (p <- ggplot(dat, aes(x=variable, y=value*100)) +
+     geom_violin(aes(fill=factor(variable)), scale='width') +
+     geom_boxplot(aes(fill=factor(variable)), width=0.1) +
+     scale_fill_manual(values = c('#00C957', '#1E90FF',
+                           '#FFA500', '#DC143C')) +
+     scale_y_continuous(limits=c(0, 100)) +
+     ylab('% Unclassified') +
+     stat_summary(fun.y = 'mean', geom = 'point',
+                  shape=15, size=4, fill= 'white') +
+     coord_flip())
+    
+    print(p + theme(panel.background=element_blank(),
+                    panel.border=element_blank(),
+                    panel.grid.minor=element_blank(),
+                    panel.grid.major=element_blank(),
+                    axis.line=element_line(),
+                    axis.text.x=element_text(size=30, face='bold'),
+                    axis.text.y=element_text(size=30, face='bold'),
+                    axis.title.x=element_text(size=30, face='bold',
+                        vjust=0.2),
+                    axis.title.y=element_blank(),
+                    legend.position='none'))
+    dev.off()
+    rm(p)
+}
